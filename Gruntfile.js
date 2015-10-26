b04301a2772ba352b9748e761880a2b0e7762f89
Coby Tamayo <acobster@gmail.com>
2015-10-26 12:37:17 -0700

    grunt-contrib-less and grunt-contrib-watch tasks to compile LESS to public CSS file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,37 @@ module.exports = function(grunt) {
 
   // Configure Grunt tasks...
   grunt.initConfig({
-		connect: {
-			server: {
-				options: {
-					port: 8080,
-					base: '.',
-					keepalive: true
-				}
-			}
-		},
-	});
+    less: {
+      stylesheet: {
+        files: {
+          'css/style.css': ['less/style.less']
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: '.'
+        }
+      }
+    },
+
+    watch: {
+      less: {
+        files: ['less/*.less'],
+        tasks: ['less']
+      }
+    },
+  });
 
   // Register Node plugins as Grunt tasks...
-	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+  grunt.loadNpmTasks( 'grunt-contrib-connect' );
+  grunt.loadNpmTasks( 'grunt-contrib-less' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
   // Default task.
-  grunt.registerTask('default', ['connect']);
+  grunt.registerTask('default', ['less', 'connect', 'watch']);
 
 };
