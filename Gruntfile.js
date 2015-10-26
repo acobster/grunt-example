22e9fa9535f83c5e403d5fd14bba93e17d94fe49
Coby Tamayo <acobster@gmail.com>
2015-10-26 12:46:12 -0700

    grunt-contrib-cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,14 @@ module.exports = function(grunt) {
       }
     },
 
+    cssmin: {
+      stylesheet: {
+        files: {
+          'css/style.min.css': ['css/style.css']
+        }
+      }
+    },
+
     connect: {
       server: {
         options: {
@@ -23,17 +31,19 @@ module.exports = function(grunt) {
     watch: {
       less: {
         files: ['less/*.less'],
-        tasks: ['less']
+        tasks: ['less', 'cssmin']
       }
     },
+
   });
 
   // Register Node plugins as Grunt tasks...
   grunt.loadNpmTasks( 'grunt-contrib-connect' );
   grunt.loadNpmTasks( 'grunt-contrib-less' );
   grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 
   // Default task.
-  grunt.registerTask('default', ['less', 'connect', 'watch']);
+  grunt.registerTask('default', ['less', 'cssmin', 'connect', 'watch']);
 
 };
